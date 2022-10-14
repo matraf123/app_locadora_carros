53452a9d3a5de70ee5081ee1adfb337098c214b7
matraf123 <matraf123@gmail.com>
2022-10-14 16:54:00 -0300

    modified:   app/Http/Controllers/MarcaController.php

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca) {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +17,9 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return $marcas;
     }
 
     /**
@@ -35,19 +40,24 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        //$marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return ['erro' => 'Recurso pesquisado não existe'];
+        }
+        return $marca;
     }
 
     /**
@@ -65,22 +75,32 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        /*
+        print_r($request->all()); //os dados atualizados
+        echo '<hr>';
+        print_r($marca->getAttributes()); //os dados antigos
+        */
+        //$marca->update($request->all());
+        $marca = $this->marca->find($id);
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        $marca->delete();
+        return ['msg' => 'A marca foi removida com sucesso!'];
     }
 }
